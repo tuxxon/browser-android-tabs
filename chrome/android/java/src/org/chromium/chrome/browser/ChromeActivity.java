15831051350189df37435de046cd3af1dfe82fbd
Serg <serg.zhukovsky@gmail.com>
2016-08-30 20:48:07 -0400

    Added blocking statistic
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -530,10 +530,11 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
                 }
                 if (currentTab.isLoading()) {
                     currentTab.stopLoading();
-                } else {
-                    currentTab.reload();
-                    RecordUserAction.record("MobileToolbarReload");
                 }
+
+                currentTab.reloadIgnoringCache();
+                RecordUserAction.record("MobileToolbarReload");
+
                 if (mBraveShieldsMenuHandler != null) mBraveShieldsMenuHandler.hideBraveShieldsMenu();
             }
         });
@@ -2373,6 +2374,12 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     @Deprecated
     public DiscardableReferencePool getReferencePool() {
         return mReferencePool;
+
+    /**
+     * Updates Bravery Panel counts
+     */
+    public void updateBraveryPanelCounts(int adsAndTrackers, int httpsUpgrades) {
+        mBraveShieldsMenuHandler.updateValues(adsAndTrackers, httpsUpgrades);
     }
 
     private void clearToolbarResourceCache() {
