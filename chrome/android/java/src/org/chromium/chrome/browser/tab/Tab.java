71967e9cacd6256b11be6c17e9c160c7697bb452
Serg <serg.zhukovsky@gmail.com>
2016-09-14 09:41:46 -0400

    Added Scripts Blocked statistic
    Auditors: @bbondy
    
    Conflicts:
    	components/content_settings/content/common/content_settings_messages.h
    	third_party/WebKit/Source/bindings/core/v8/ScriptController.cpp
    	third_party/WebKit/Source/core/dom/ScriptLoader.cpp
    	third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
    	third_party/WebKit/Source/core/loader/FrameLoaderClient.h
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.h
    	third_party/WebKit/public/web/WebContentSettingsClient.h
    
    Conflicts:
    	chrome/browser/content_settings/tab_specific_content_settings.cc
    	chrome/renderer/content_settings_observer.h
    	third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/public/web/WebContentSettingsClient.h

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -414,6 +414,7 @@ public class Tab
     private boolean mControlsResizeView;
     private int mAdsAndTrackers;
     private int mHttpsUpgrades;
+    private int mScriptsBlocked;
 
     /**
      * The publisher URL for pages hosted on a trusted CDN, or null otherwise.
@@ -649,6 +650,7 @@ public class Tab
         };
         mAdsAndTrackers = 0;
         mHttpsUpgrades = 0;
+        mScriptsBlocked = 0;
     }
 
     private int calculateDefaultThemeColor() {
@@ -3494,9 +3496,11 @@ public class Tab
         tracker.dismissed(FeatureConstants.MEDIA_DOWNLOAD_FEATURE);
         nativeMediaDownloadInProductHelpDismissed(mNativeTabAndroid);
 
-    public void braveShieldsCountUpdate(int adsAndTrackers, int httpsUpgrades) {
+    public void braveShieldsCountUpdate(int adsAndTrackers, int httpsUpgrades
+            , int scriptsBlocked) {
         mAdsAndTrackers += adsAndTrackers;
         mHttpsUpgrades += httpsUpgrades;
+        mScriptsBlocked += scriptsBlocked;
     }
 
     public int getAdsAndTrackers() {
@@ -3507,9 +3511,14 @@ public class Tab
         return mHttpsUpgrades;
     }
 
+    public int getScriptsBlocked() {
+        return mScriptsBlocked;
+    }
+
     public void clearBraveShieldsCount() {
         mAdsAndTrackers = 0;
         mHttpsUpgrades = 0;
+        mScriptsBlocked = 0;
     }
 
     /**
