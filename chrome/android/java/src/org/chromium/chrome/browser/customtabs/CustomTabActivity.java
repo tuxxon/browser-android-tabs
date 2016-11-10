ed16011215a825128a9187bc09a8816f6b94f2db
Serg <serg.zhukovsky@gmail.com>
2016-11-10 14:22:11 -0500

    Added Canvas and WebGL fingerprinting protection
    Auditors: @bbondy
    
    Conflicts:
    	chrome/common/render_messages.h
    	components/content_settings/content/common/content_settings_messages.h
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
    	third_party/WebKit/Source/core/loader/FrameLoaderClient.h
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.h
    	third_party/WebKit/public/web/WebContentSettingsClient.h
    
    Conflicts:
    	chrome/browser/renderer_host/chrome_render_message_filter.cc
    	chrome/browser/renderer_host/chrome_render_message_filter.h
    
    Conflicts:
    	chrome/browser/content_settings/tab_specific_content_settings.cc
    	chrome/browser/renderer_host/chrome_render_message_filter.h
    	chrome/renderer/content_settings_observer.h
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/public/web/WebContentSettingsClient.h

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -396,8 +396,8 @@ public class CustomTabActivity extends ChromeActivity {
 
             @Override
             public void onBraveShieldsCountUpdate(String url, int adsAndTrackers, int httpsUpgrades,
-                    int scriptsBlocked) {
-                braveShieldsCountUpdate(url, adsAndTrackers, httpsUpgrades, scriptsBlocked);
+                    int scriptsBlocked, int fingerprintsBlocked) {
+                braveShieldsCountUpdate(url, adsAndTrackers, httpsUpgrades, scriptsBlocked, fingerprintsBlocked);
             }
         };
 
@@ -525,7 +525,8 @@ public class CustomTabActivity extends ChromeActivity {
                           , url.getHost()
                           , currentTab.getAdsAndTrackers()
                           , currentTab.getHttpsUpgrades()
-                          , currentTab.getScriptsBlocked());
+                          , currentTab.getScriptsBlocked()
+                          , currentTab.getFingerprintsBlocked());
                     } catch (Exception e) {
                         setBraveShieldsBlackAndWhite();
                     }
--- third_party/blink/renderer/core/frame/local_frame_client.h
+++ third_party/blink/renderer/core/frame/local_frame_client.h
@@ -273,6 +273,8 @@ class CORE_EXPORT LocalFrameClient : public FrameClient {
 
   virtual void DidChangeScrollOffset() {}
   virtual void DidUpdateCurrentHistoryItem() {}
+
+  virtual bool AllowFingerprinting() { return true; }
   // This callback notifies the client that script was blocked
   virtual void DeniedScript() { }
 
@@ -281,6 +283,9 @@ class CORE_EXPORT LocalFrameClient : public FrameClient {
   virtual bool AllowContentInitiatedDataUrlNavigations(const KURL&) {
     return false;
   }
+
+  // This callback notifies the client that fingerprinting was blocked
+  virtual void DeniedFingerprinting() { }
 
   virtual WebCookieJar* CookieJar() const = 0;
 
