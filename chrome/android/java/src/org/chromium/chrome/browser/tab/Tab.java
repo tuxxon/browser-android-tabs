a5dd88bf44026cded24c0d7c4506dd17d757592c
Serg <serg.zhukovsky@gmail.com>
2016-11-10 14:22:11 -0500

    Added Canvas and WebGL fingerprinting protection
    Auditors: @bbondy
    
    Conflicts:
    	chrome/common/render_messages.h
    	components/content_settings/content/common/content_settings_messages.h
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
    	third_party/WebKit/Source/core/loader/FrameLoaderClient.h
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.h
    	third_party/WebKit/public/web/WebContentSettingsClient.h
    
    Conflicts:
    	chrome/browser/renderer_host/chrome_render_message_filter.cc
    	chrome/browser/renderer_host/chrome_render_message_filter.h
    
    Conflicts:
    	chrome/browser/content_settings/tab_specific_content_settings.cc
    	chrome/browser/renderer_host/chrome_render_message_filter.h
    	chrome/renderer/content_settings_observer.h
    	third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
    	third_party/WebKit/Source/web/FrameLoaderClientImpl.cpp
    	third_party/WebKit/public/web/WebContentSettingsClient.h

--- chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/Tab.java
@@ -415,6 +415,7 @@ public class Tab
     private int mAdsAndTrackers;
     private int mHttpsUpgrades;
     private int mScriptsBlocked;
+    private int mFingerprintsBlocked;
 
     /**
      * The publisher URL for pages hosted on a trusted CDN, or null otherwise.
@@ -651,6 +652,7 @@ public class Tab
         mAdsAndTrackers = 0;
         mHttpsUpgrades = 0;
         mScriptsBlocked = 0;
+        mFingerprintsBlocked = 0;
     }
 
     private int calculateDefaultThemeColor() {
@@ -3497,10 +3499,11 @@ public class Tab
         nativeMediaDownloadInProductHelpDismissed(mNativeTabAndroid);
 
     public void braveShieldsCountUpdate(int adsAndTrackers, int httpsUpgrades
-            , int scriptsBlocked) {
+            , int scriptsBlocked, int fingerprintsBlocked) {
         mAdsAndTrackers += adsAndTrackers;
         mHttpsUpgrades += httpsUpgrades;
         mScriptsBlocked += scriptsBlocked;
+        mFingerprintsBlocked += fingerprintsBlocked;
     }
 
     public int getAdsAndTrackers() {
@@ -3515,10 +3518,15 @@ public class Tab
         return mScriptsBlocked;
     }
 
+    public int getFingerprintsBlocked() {
+        return mFingerprintsBlocked;
+    }
+
     public void clearBraveShieldsCount() {
         mAdsAndTrackers = 0;
         mHttpsUpgrades = 0;
         mScriptsBlocked = 0;
+        mFingerprintsBlocked = 0;
     }
 
     /**
