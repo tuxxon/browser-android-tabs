8c8d11219c29fde9e7d9e88248b8133daf38c6ed
Serg <serg.zhukovsky@gmail.com>
2017-02-07 12:26:38 -0500

    Made it buildable after merge
    Conflicts:
    	chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
    	chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
    	chrome/android/java/strings/translations/android_chrome_strings_ar.xtb
    	components/strings/components_strings_el.xtb
    	components/strings/components_strings_mr.xtb

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -299,6 +299,7 @@ public class NewTabPageView
                 background.setCornerRadius(mSearchBoxView.getLayoutParams().height / 2.f);
             }
         }
+
         mNoSearchLogoSpacer = mNewTabPageLayout.findViewById(R.id.no_search_logo_spacer);
 
         mSnapScrollRunnable = new SnapScrollRunnable();
