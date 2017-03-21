f7de06c356293d6c69b2cf5d84d6f0e042f67aa5
Serg <serg.zhukovsky@gmail.com>
2017-03-21 17:35:33 -0400

    Moved StatUpdater on the top level of onResume event

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -17,6 +17,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -97,6 +98,7 @@ import org.chromium.chrome.browser.init.ProcessInitializationHandler;
 import org.chromium.chrome.browser.locale.LocaleManager;
 import org.chromium.chrome.browser.media.PictureInPicture;
 import org.chromium.chrome.browser.media.PictureInPictureController;
+import org.chromium.chrome.browser.init.StatsUpdater;
 import org.chromium.chrome.browser.metrics.LaunchMetrics;
 import org.chromium.chrome.browser.metrics.StartupMetrics;
 import org.chromium.chrome.browser.metrics.UmaSessionStats;
@@ -184,6 +186,32 @@ import javax.annotation.Nullable;
 public abstract class ChromeActivity extends AsyncInitializationActivity
         implements TabCreatorManager, AccessibilityStateChangeListener, PolicyChangeListener,
         ContextualSearchTabPromotionDelegate, SnackbarManageable, SceneChangeObserver {
+
+    // Stats update
+    class UpdateStatsAsyncTask extends AsyncTask<Void,Void,Long> {
+
+        private Context mContext = null;
+
+        public UpdateStatsAsyncTask(Context context) {
+            mContext = context;
+        }
+
+        protected Long doInBackground(Void... params) {
+            if (null == mContext) {
+                return null;
+            }
+            try {
+                StatsUpdater.UpdateStats(mContext);
+            }
+            catch(Exception exc) {
+                // Just ignore it if we cannot update
+            }
+
+            return null;
+        }
+    }
+
+
     /**
      * Factory which creates the AppMenuHandler.
      */
@@ -966,6 +994,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     @Override
     public void onResumeWithNative() {
         super.onResumeWithNative();
+        new UpdateStatsAsyncTask(this).execute();
         markSessionResume();
         RecordUserAction.record("MobileComeToForeground");
 
