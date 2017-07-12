00bba3cac4a081d6eac41a739ce252af923a9c85
SergeyZhukovsky <serg.zhukovsky@gmail.com>
2017-07-12 14:20:31 -0400

    MixPanel integration

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -46,6 +46,8 @@ import org.chromium.chrome.browser.vr_shell.OnExitVrRequestListener;
 import org.chromium.chrome.browser.vr_shell.VrIntentUtils;
 import org.chromium.chrome.browser.vr_shell.VrShellDelegate;
 
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
 /**
  * Basic application functionality that should be shared among all browser applications that use
  * chrome layer.
@@ -62,6 +64,8 @@ public class ChromeApplication extends Application {
     public BraveSyncWorker mBraveSyncWorker = null;
     // Stats updater
     public StatsUpdaterWorker mStatsUpdaterWorker = null;
+    // Mixpanel global object
+    public MixpanelAPI mMixpanelInstance = null;
 
     // Called by the framework for ALL processes. Runs before ContentProviders are created.
     // Quirk: context.getApplicationContext() returns null during this method.
