7d402991a2c9bcb0ee626dab5fa3c61465e90ec9
samartnik <artem@brave.com>
2017-08-04 14:41:46 +0300

    New Tab Page with Brave stats

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -287,7 +287,8 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
   if (enable_tracking_protection_ && !shieldsSetExplicitly) {
     isTPEnabled = enable_tracking_protection_->GetValue();
   }
-  int adsAndTrackersBlocked = 0;
+  int adsBlocked = 0;
+  int trackersBlocked = 0;
   int httpsUpgrades = 0;
 	if (request
       && !firstPartyUrl
@@ -300,7 +301,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 					request->url().host())
 				) {
 		block = true;
-    adsAndTrackersBlocked++;
+    trackersBlocked++;
 	}
   bool isAdBlockEnabled = true;
   if (enable_ad_block_ && !shieldsSetExplicitly) {
@@ -326,7 +327,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 					(unsigned int)info->GetResourceType(),
           isAdBlockRegionalEnabled)) {
 		block = true;
-    adsAndTrackersBlocked++;
+    adsBlocked++;
 	}
   bool check_httpse_redirect = true;
   if (block && info && content::RESOURCE_TYPE_IMAGE == info->GetResourceType()) {
@@ -352,9 +353,10 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
     }
   }
   //
-  if (nullptr != shieldsConfig && (0 != adsAndTrackersBlocked || 0 != httpsUpgrades)) {
+  if (nullptr != shieldsConfig && (0 != trackersBlocked || 0 != adsBlocked || 0 != httpsUpgrades)) {
     shieldsConfig->setBlockedCountInfo(last_first_party_url_.spec()
-        , adsAndTrackersBlocked
+        , trackersBlocked
+        , adsBlocked
         , httpsUpgrades
         , 0
         , 0);
