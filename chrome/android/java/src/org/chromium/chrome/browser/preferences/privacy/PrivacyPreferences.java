022edff0fbb8c87637b466d4c921f40005f3f51c
samartnik <artem@brave.com>
2017-08-31 17:26:09 +0300

    Additional changes according to proposed modifications

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.preferences.privacy;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
@@ -16,6 +17,7 @@ import android.view.MenuItem;
 
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
+import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchFieldTrial;
 import org.chromium.chrome.browser.help.HelpAndFeedback;
@@ -48,6 +50,7 @@ public class PrivacyPreferences extends PreferenceFragment
     private static final String PREF_CONTEXTUAL_SEARCH = "contextual_search";
     private static final String PREF_NETWORK_PREDICTIONS = "network_predictions";
     private static final String PREF_CLEAR_BROWSING_DATA = "clear_browsing_data";
+    private static final String PREF_SEND_METRICS = "send_metrics";
     //private static final String PREF_DO_NOT_TRACK = "do_not_track";
     //private static final String PREF_USAGE_AND_CRASH_REPORTING = "usage_and_crash_reports";
     //private static final String PREF_PHYSICAL_WEB = "physical_web";
@@ -141,6 +144,11 @@ public class PrivacyPreferences extends PreferenceFragment
         adBlockRegionalPref.setOnPreferenceChangeListener(this);
         adBlockRegionalPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
 
+        ChromeBaseCheckBoxPreference sendMetricsPref =
+                (ChromeBaseCheckBoxPreference) findPreference(PREF_SEND_METRICS);
+        trackingProtectionPref.setOnPreferenceChangeListener(this);
+        trackingProtectionPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
+
         updateSummaries();
     }
 
@@ -178,6 +186,10 @@ public class PrivacyPreferences extends PreferenceFragment
         } else if (PREF_CAN_MAKE_PAYMENT.equals(key)) {
             PrefServiceBridge.getInstance().setBoolean(
                     Pref.CAN_MAKE_PAYMENT_ENABLED, (boolean) newValue);
+        } else if (PREF_SEND_METRICS.equals(key)) {
+            SharedPreferences.Editor sharedPreferencesEditor = ContextUtils.getAppSharedPreferences().edit();
+            sharedPreferencesEditor.putBoolean(PREF_SEND_METRICS, (boolean)newValue);
+            sharedPreferencesEditor.apply();
         }
 
         return true;
