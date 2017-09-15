aa8980110a41ac03c73bca61f1702f36b5dd1457
Alexey <alexey.barabash@gmail.com>
2017-09-15 16:32:53 +0300

    Desktop mode settings after re-base 1

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/SingleCategoryPreferences.java
@@ -465,6 +465,8 @@ public class SingleCategoryPreferences extends PreferenceFragment
                 PrefServiceBridge.getInstance().setSoundEnabled((boolean) newValue);
             } else if (mCategory.showUsbDevices()) {
                 PrefServiceBridge.getInstance().setUsbEnabled((boolean) newValue);
+            } else if (mCategory.showDesktopViewSites()) {
+                PrefServiceBridge.getInstance().setDesktopViewEnabled((boolean) newValue);
             }
 
             // Categories that support adding exceptions also manage the 'Add site' preference.
@@ -844,6 +846,8 @@ public class SingleCategoryPreferences extends PreferenceFragment
                     globalToggle.setChecked(PrefServiceBridge.getInstance().isSoundEnabled());
                 } else if (mCategory.showUsbDevices()) {
                     globalToggle.setChecked(PrefServiceBridge.getInstance().isUsbEnabled());
+                } else if (mCategory.showDesktopViewSites()) {
+                    globalToggle.setChecked(PrefServiceBridge.getInstance().desktopViewEnabled());
                 }
             }
         }
