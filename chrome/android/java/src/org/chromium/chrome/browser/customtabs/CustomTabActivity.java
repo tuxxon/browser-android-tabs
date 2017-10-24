5606d7a63c40af04b6766ae3bcd109d09daa30e4
AlexeyBarabash <alexey@brave.com>
2017-10-24 15:55:16 +0300

    re-merge after rebase for IncognitoShields issue

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -411,7 +411,7 @@ public class CustomTabActivity extends ChromeActivity {
                 if (getActivityTab() == tab) {
                     try {
                         URL urlCheck = new URL(url);
-                        setBraveShieldsColor(urlCheck.getHost());
+                        setBraveShieldsColor(tab.isIncognito(), urlCheck.getHost());
                     } catch (Exception e) {
                         setBraveShieldsBlackAndWhite();
                     }
@@ -425,7 +425,7 @@ public class CustomTabActivity extends ChromeActivity {
                 if (getActivityTab() == tab) {
                     try {
                         URL urlCheck = new URL(url);
-                        setBraveShieldsColor(urlCheck.getHost());
+                        setBraveShieldsColor(tab.isIncognito(), urlCheck.getHost());
                     } catch (Exception e) {
                         setBraveShieldsBlackAndWhite();
                     }
@@ -522,8 +522,9 @@ public class CustomTabActivity extends ChromeActivity {
                     try {
                         URL url = new URL(currentTab.getUrl());
 
-                        setBraveShieldsColor(url.getHost());
+                        setBraveShieldsColor(currentTab.isIncognito(), url.getHost());
                         getBraveShieldsMenuHandler().show((View)findViewById(R.id.brave_shields_button)
+                          , currentTab.isIncognito()
                           , url.getHost()
                           , currentTab.getAdsAndTrackers()
                           , currentTab.getHttpsUpgrades()
