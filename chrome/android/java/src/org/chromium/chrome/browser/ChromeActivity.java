b081d448cdb02f93f39cc70721b1b9ae1b190edc
samartnik <artem@brave.com>
2018-02-19 10:24:49 +0200

    Sync flow (intermediate commit)

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1434,7 +1434,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
         // Starting Brave Sync
         ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
-            //app.mBraveSyncWorker = new BraveSyncWorker(this);
+            app.mBraveSyncWorker = new BraveSyncWorker(this);
             app.mStatsUpdaterWorker = new StatsUpdaterWorker(this);
             app.mADBlockUpdaterWorker = new ADBlockUpdaterWorker(this);
             MixPanelWorker.SendBraveAppStartEvent();
