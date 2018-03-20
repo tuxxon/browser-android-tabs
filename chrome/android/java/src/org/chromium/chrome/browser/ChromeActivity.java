60f72dfac8e3e698ea952af3fa68afa22bd73490
samartnik <artem@brave.com>
2018-03-20 10:46:02 +0200

    Moved shields initialization

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -207,6 +207,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             mContext = context;
         }
 
+        @Override
         protected Long doInBackground(Void... params) {
             if (null == mContext) {
                 return null;
--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1667,6 +1667,7 @@ public class ChromeTabbedActivity
         });
     }
 
+    @Override
     protected void setBraveShieldsColor(boolean incognitoTab, String url) {
         ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
         if (null != app) {
