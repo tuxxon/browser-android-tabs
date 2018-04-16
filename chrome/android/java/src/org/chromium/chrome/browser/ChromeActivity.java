3836f8afd832ec6901666b7ef75e41e7fc1aff40
samartnik <artem@brave.com>
2018-04-16 17:10:10 +0300

    Disabled metrics code

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1407,10 +1407,10 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             app.mADBlockUpdaterWorker.Stop();
             app.mADBlockUpdaterWorker = null;
         }
-        if (null != app && null != app.mMixpanelInstance) {
+        /*if (null != app && null != app.mMixpanelInstance) {
             app.mMixpanelInstance.flush();
             app.mMixpanelInstance = null;
-        }
+        }*/
 
 
         if (mTabContentManager != null) {
@@ -1556,7 +1556,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             //app.mBraveSyncWorker = new BraveSyncWorker(this);
             app.mStatsUpdaterWorker = new StatsUpdaterWorker(this);
             app.mADBlockUpdaterWorker = new ADBlockUpdaterWorker(this);
-            MixPanelWorker.SendBraveAppStartEvent();
+            //MixPanelWorker.SendBraveAppStartEvent();
         }
     }
 
@@ -2256,7 +2256,7 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             }
             RecordUserAction.record("MobileMenuHistory");
             HistoryManagerUtils.showHistoryManager(this, currentTab);
-            MixPanelWorker.SendEvent("History Opened");
+            //MixPanelWorker.SendEvent("History Opened");
         } else if (id == R.id.share_menu_id || id == R.id.direct_share_menu_id) {
             onShareMenuItemSelected(id == R.id.direct_share_menu_id,
                     getCurrentTabModel().isIncognito());
