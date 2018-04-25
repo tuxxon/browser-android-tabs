6c069fd7c4c5b7a223e060285bd299018b435a42
samartnik <artem@brave.com>
2018-04-25 19:08:52 +0300

    Fixed custom tab view

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -354,7 +354,7 @@ public class CustomTabActivity extends ChromeActivity {
     public void postInflationStartup() {
         super.postInflationStartup();
 
-        /*getToolbarManager().setCloseButtonDrawable(mIntentDataProvider.getCloseButtonDrawable());
+        getToolbarManager().setCloseButtonDrawable(mIntentDataProvider.getCloseButtonDrawable());
         getToolbarManager().setShowTitle(mIntentDataProvider.getTitleVisibilityState()
                 == CustomTabsIntent.SHOW_PAGE_TITLE);
         if (mConnection.shouldHideDomainForSession(mSession)) {
@@ -380,9 +380,9 @@ public class CustomTabActivity extends ChromeActivity {
 
         // Setting task title and icon to be null will preserve the client app's title and icon.
         ApiCompatibilityUtils.setTaskDescription(this, null, null, toolbarColor);
-        showCustomButtonOnToolbar();
+        showCustomButtonsOnToolbar();
         mBottomBarDelegate = new CustomTabBottomBarDelegate(this, mIntentDataProvider, getFullscreenManager());
-        mBottomBarDelegate.showBottomBarIfNecessary();*/
+        mBottomBarDelegate.showBottomBarIfNecessary();
     }
 
     @Override
