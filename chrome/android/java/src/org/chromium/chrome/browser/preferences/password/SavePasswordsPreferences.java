4549c8f2dd2e58de9d5242c7962ed15a6148fa91
samartnik <artem@brave.com>
2018-05-30 13:52:31 +0300

    Removed google passwords link

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferences.java
@@ -708,7 +708,7 @@ public class SavePasswordsPreferences
             return;
         }
 
-        displayManageAccountLink();
+        //displayManageAccountLink();
 
         PreferenceGroup passwordParent;
         if (mSearchQuery == null) {
@@ -783,7 +783,7 @@ public class SavePasswordsPreferences
             return;
         }
 
-        displayManageAccountLink();
+        //displayManageAccountLink();
 
         PreferenceCategory profileCategory = new PreferenceCategory(getActivity());
         profileCategory.setKey(PREF_KEY_CATEGORY_EXCEPTIONS);
