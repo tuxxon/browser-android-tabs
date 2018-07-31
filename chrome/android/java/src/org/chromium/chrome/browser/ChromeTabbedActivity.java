8ce45137779042bd6697583594586575652ff51d
samartnik <artem@brave.com>
2018-07-31 19:12:32 +0300

    Fixed build for cr-68

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1492,7 +1492,12 @@ public class ChromeTabbedActivity
                     // we're in incognito mode.
                     setStatusBarColor(null, Color.BLACK);
                 }
+            }
+        });
 
+        mTabModelSelectorTabObserver = new TabModelSelectorTabObserver(mTabModelSelectorImpl) {
+            @Override
+            public void onPageLoadStarted(Tab tab, String url) {
                 ChromeApplication app = (ChromeApplication)ContextUtils.getApplicationContext();
                 if ((null != app) && (null != app.getShieldsConfig())) {
                     app.getShieldsConfig().setTabModelSelectorTabObserver(mTabModelSelectorTabObserver);
@@ -1509,7 +1514,7 @@ public class ChromeTabbedActivity
             }
 
             @Override
-            public void onPageLoadFinished(Tab tab) {
+            public void onPageLoadFinished(final Tab tab) {
                 String url = tab.getUrl();
                 if (getActivityTab() == tab) {
                     try {
@@ -1519,12 +1524,6 @@ public class ChromeTabbedActivity
                         setBraveShieldsBlackAndWhite();
                     }
                 }
-            }
-        });
-
-        mTabModelSelectorTabObserver = new TabModelSelectorTabObserver(mTabModelSelectorImpl) {
-            @Override
-            public void onPageLoadFinished(final Tab tab) {
                 mAppIndexingUtil.extractCopylessPasteMetadata(tab);
             }
 
