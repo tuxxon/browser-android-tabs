d3f981431e881272143b939b649db8b8353725f5
samartnik <artem@brave.com>
2018-08-20 15:23:11 +0300

    Implemented referrer ad blocking

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -1370,6 +1370,8 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
                                              AXTreeSnapshotCombiner* combiner,
                                              ui::AXMode ax_mode);
 
+  void ShouldBlockReferrer(NavigationHandle* navigation_handle);
+
   // Data for core operation ---------------------------------------------------
 
   // Delegate for notifying our owner about stuff. Not owned by us.
