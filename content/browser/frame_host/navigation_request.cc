cc46b911b062230375a2bc4b2ceca667a55d0e70
samartnik <artem@brave.com>
2018-09-05 12:23:18 +0300

    Disabled referrer blocking. Needs more investigation.

--- content/browser/frame_host/navigation_request.cc
+++ content/browser/frame_host/navigation_request.cc
@@ -1911,7 +1911,7 @@ void NavigationRequest::IgnorePipeDisconnection() {
 }
 
 void NavigationRequest::ShouldBlockReferrer() {
-  if (!frame_tree_node_) {
+  /*if (!frame_tree_node_) {
     return;
   }
   auto* navigation_entry =
@@ -1960,7 +1960,7 @@ void NavigationRequest::ShouldBlockReferrer() {
           common_params_.url.GetOrigin(),
           original_referrer.policy, &new_referrer)) {
     common_params_.referrer = new_referrer;
-  }
+  }*/
 }
 
 }  // namespace content
