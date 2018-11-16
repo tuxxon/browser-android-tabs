6a0f8daf5f8ec18db9f2fcc07bd928c9c53071f3
AlexeyBarabash <alexey@brave.com>
2018-11-16 15:29:23 +0200

    Display chrome:// urls as brave:// in the LocationBar (brave-core PR 584)

---
+++ brave/components/toolbar/brave_toolbar_model_impl.h
@@ -0,0 +1,20 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef BRAVE_COMPONENTS_TOOLBAR_BRAVE_TOOLBAR_MODEL_IMPL_H_
+#define BRAVE_COMPONENTS_TOOLBAR_BRAVE_TOOLBAR_MODEL_IMPL_H_
+
+#include "components/toolbar/toolbar_model_impl.h"
+
+class BraveToolbarModelImpl : public ToolbarModelImpl {
+  public:
+    using ToolbarModelImpl::ToolbarModelImpl;
+    // On Android GetURLForDisplay is not invoked as on brave-coer,
+    // but GetFormattedFullURL is invoked instead
+    base::string16 GetFormattedFullURL() const override;
+  private:
+    DISALLOW_COPY_AND_ASSIGN(BraveToolbarModelImpl);
+};
+
+#endif
