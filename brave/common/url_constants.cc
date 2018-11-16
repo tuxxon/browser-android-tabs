420d5d9a72fc51e46f41241c6866ae7eab94f74f
AlexeyBarabash <alexey@brave.com>
2018-11-16 12:15:13 +0200

    Handle and redirect brave:// to chrome:// (brave-core PR 164)

---
+++ brave/common/url_constants.cc
@@ -0,0 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/common/url_constants.h"
+
+const char kBraveUIScheme[] = "brave";
